b0aa324fa09bb74fbe1ad1f6daeae419b9ec9c11
Rizki Muh Pasinringi <rizkimuhpasinringi@gmail.com>
2022-06-06 08:57:22 +0800

    jpg to png

--- Js/MobileLegends.js
+++ Js/MobileLegends.js
@@ -393,18 +393,448 @@ nominal_bni18.addEventListener('click', () => {
 })
 
 // VIRTUAL ACCOUNT
+// ALL
 const virtualAcc = document.getElementById('virtualAccount');
+
+// mayBankVa
 const maybankVa = document.getElementById('maybankVa');
 
 const textNominalMaybankVa = maybankVa.textContent; // biar presist
 
-const maybank_Va = document.getElementById('1');
-maybank_Va.addEventListener('click', () => {
+// 1
+const maybank_Va1 = document.getElementById('1');
+maybank_Va1.addEventListener('click', () => {
     harga += 117000;
 
-    if (maybank_Va.checked) {
+    if (maybank_Va1.checked) {
         maybankVa.textContent = textNominalMaybankVa + 'Rp.' + harga;
         virtualAcc.textContent = textNominalMaybankVa + 'Rp.' + harga;
+        harga = 0; // set ulang biar g nimpa 
+    }
+})
+
+// 2
+const maybank_Va2 = document.getElementById('2');
+maybank_Va2.addEventListener('click', () => {
+    harga += 117000;
+
+    if (maybank_Va2.checked) {
+        maybankVa.textContent = textNominalMaybankVa + 'Rp.' + harga;
+        virtualAcc.textContent = textNominalMaybankVa + 'Rp.' + harga;
+        harga = 0; // set ulang biar g nimpa 
+    }
+})
+
+// 3
+const maybank_Va3 = document.getElementById('3');
+maybank_Va3.addEventListener('click', () => {
+    harga += 117000;
+
+    if (maybank_Va3.checked) {
+        maybankVa.textContent = textNominalMaybankVa + 'Rp.' + harga;
+        virtualAcc.textContent = textNominalMaybankVa + 'Rp.' + harga;
+        harga = 0; // set ulang biar g nimpa 
+    }
+})
+
+// 4
+const maybank_Va4 = document.getElementById('4');
+maybank_Va4.addEventListener('click', () => {
+    harga += 117000;
+
+    if (maybank_Va4.checked) {
+        maybankVa.textContent = textNominalMaybankVa + 'Rp.' + harga;
+        virtualAcc.textContent = textNominalMaybankVa + 'Rp.' + harga;
+        harga = 0; // set ulang biar g nimpa 
+    }
+})
+
+// 5
+const maybank_Va5 = document.getElementById('5');
+maybank_Va5.addEventListener('click', () => {
+    harga += 117000;
+
+    if (maybank_Va5.checked) {
+        maybankVa.textContent = textNominalMaybankVa + 'Rp.' + harga;
+        virtualAcc.textContent = textNominalMaybankVa + 'Rp.' + harga;
+        harga = 0; // set ulang biar g nimpa 
+    }
+})
+
+// 6
+const maybank_Va6 = document.getElementById('6');
+maybank_Va6.addEventListener('click', () => {
+    harga += 117000;
+
+    if (maybank_Va6.checked) {
+        maybankVa.textContent = textNominalMaybankVa + 'Rp.' + harga;
+        virtualAcc.textContent = textNominalMaybankVa + 'Rp.' + harga;
+        harga = 0; // set ulang biar g nimpa 
+    }
+})
+
+// 7
+const maybank_Va7 = document.getElementById('7');
+maybank_Va7.addEventListener('click', () => {
+    harga += 117000;
+
+    if (maybank_Va7.checked) {
+        maybankVa.textContent = textNominalMaybankVa + 'Rp.' + harga;
+        virtualAcc.textContent = textNominalMaybankVa + 'Rp.' + harga;
+        harga = 0; // set ulang biar g nimpa 
+    }
+})
+
+// 8
+const maybank_Va8 = document.getElementById('8');
+maybank_Va8.addEventListener('click', () => {
+    harga += 117000;
+
+    if (maybank_Va8.checked) {
+        maybankVa.textContent = textNominalMaybankVa + 'Rp.' + harga;
+        virtualAcc.textContent = textNominalMaybankVa + 'Rp.' + harga;
+        harga = 0; // set ulang biar g nimpa 
+    }
+})
+
+// 9
+const maybank_Va9 = document.getElementById('9');
+maybank_Va9.addEventListener('click', () => {
+    harga += 117000;
+
+    if (maybank_Va9.checked) {
+        maybankVa.textContent = textNominalMaybankVa + 'Rp.' + harga;
+        virtualAcc.textContent = textNominalMaybankVa + 'Rp.' + harga;
+        harga = 0; // set ulang biar g nimpa 
+    }
+})
+
+// 10
+const maybank_Va10 = document.getElementById('10');
+maybank_Va10.addEventListener('click', () => {
+    harga += 117000;
+
+    if (maybank_Va10.checked) {
+        maybankVa.textContent = textNominalMaybankVa + 'Rp.' + harga;
+        virtualAcc.textContent = textNominalMaybankVa + 'Rp.' + harga;
+        harga = 0; // set ulang biar g nimpa 
+    }
+})
+
+// 11
+const maybank_Va11 = document.getElementById('11');
+maybank_Va11.addEventListener('click', () => {
+    harga += 117000;
+
+    if (maybank_Va11.checked) {
+        maybankVa.textContent = textNominalMaybankVa + 'Rp.' + harga;
+        virtualAcc.textContent = textNominalMaybankVa + 'Rp.' + harga;
+        harga = 0; // set ulang biar g nimpa 
+    }
+})
+
+// 12
+const maybank_Va12 = document.getElementById('12');
+maybank_Va12.addEventListener('click', () => {
+    harga += 117000;
+
+    if (maybank_Va12.checked) {
+        maybankVa.textContent = textNominalMaybankVa + 'Rp.' + harga;
+        virtualAcc.textContent = textNominalMaybankVa + 'Rp.' + harga;
+        harga = 0; // set ulang biar g nimpa 
+    }
+})
+
+// 13
+const maybank_Va13 = document.getElementById('13');
+maybank_Va13.addEventListener('click', () => {
+    harga += 117000;
+
+    if (maybank_Va13.checked) {
+        maybankVa.textContent = textNominalMaybankVa + 'Rp.' + harga;
+        virtualAcc.textContent = textNominalMaybankVa + 'Rp.' + harga;
+        harga = 0; // set ulang biar g nimpa 
+    }
+})
+
+// 14
+const maybank_Va14 = document.getElementById('14');
+maybank_Va14.addEventListener('click', () => {
+    harga += 117000;
+
+    if (maybank_Va14.checked) {
+        maybankVa.textContent = textNominalMaybankVa + 'Rp.' + harga;
+        virtualAcc.textContent = textNominalMaybankVa + 'Rp.' + harga;
+        harga = 0; // set ulang biar g nimpa 
+    }
+})
+
+// 15
+const maybank_Va15 = document.getElementById('15');
+maybank_Va15.addEventListener('click', () => {
+    harga += 117000;
+
+    if (maybank_Va15.checked) {
+        maybankVa.textContent = textNominalMaybankVa + 'Rp.' + harga;
+        virtualAcc.textContent = textNominalMaybankVa + 'Rp.' + harga;
+        harga = 0; // set ulang biar g nimpa 
+    }
+})
+
+// 16
+const maybank_Va16 = document.getElementById('16');
+maybank_Va16.addEventListener('click', () => {
+    harga += 117000;
+
+    if (maybank_Va16.checked) {
+        maybankVa.textContent = textNominalMaybankVa + 'Rp.' + harga;
+        virtualAcc.textContent = textNominalMaybankVa + 'Rp.' + harga;
+        harga = 0; // set ulang biar g nimpa 
+    }
+})
+
+// 17
+const maybank_Va17 = document.getElementById('17');
+maybank_Va17.addEventListener('click', () => {
+    harga += 117000;
+
+    if (maybank_Va17.checked) {
+        maybankVa.textContent = textNominalMaybankVa + 'Rp.' + harga;
+        virtualAcc.textContent = textNominalMaybankVa + 'Rp.' + harga;
+        harga = 0; // set ulang biar g nimpa 
+    }
+})
+
+// 18
+const maybank_Va18 = document.getElementById('18');
+maybank_Va18.addEventListener('click', () => {
+    harga += 117000;
+
+    if (maybank_Va18.checked) {
+        maybankVa.textContent = textNominalMaybankVa + 'Rp.' + harga;
+        virtualAcc.textContent = textNominalMaybankVa + 'Rp.' + harga;
+        harga = 0; // set ulang biar g nimpa 
+    }
+})
+
+// permataVa
+// all
+const permataVa = document.getElementById('permataVa');
+
+const textNominalpermataVa = permataVa.textContent; // biar presist
+
+// 1
+const permata_Va = document.getElementById('1');
+permata_Va.addEventListener('click', () => {
+    harga += 117000;
+
+    if (permata_Va.checked) {
+        permataVa.textContent = textNominalpermataVa + 'Rp.' + harga;
+        virtualAcc.textContent = textNominalpermataVa + 'Rp.' + harga;
+        harga = 0; // set ulang biar g nimpa 
+    }
+})
+
+// 2
+const permata_Va2 = document.getElementById('2');
+permata_Va2.addEventListener('click', () => {
+    harga += 117000;
+
+    if (permata_Va2.checked) {
+        permataVa.textContent = textNominalpermataVa + 'Rp.' + harga;
+        virtualAcc.textContent = textNominalpermataVa + 'Rp.' + harga;
+        harga = 0; // set ulang biar g nimpa 
+    }
+})
+
+// 3
+const permata_Va3 = document.getElementById('3');
+permata_Va3.addEventListener('click', () => {
+    harga += 117000;
+
+    if (permata_Va3.checked) {
+        permataVa.textContent = textNominalpermataVa + 'Rp.' + harga;
+        virtualAcc.textContent = textNominalpermataVa + 'Rp.' + harga;
+        harga = 0; // set ulang biar g nimpa 
+    }
+})
+
+// 4
+const permata_Va4 = document.getElementById('4');
+permata_Va4.addEventListener('click', () => {
+    harga += 117000;
+
+    if (permata_Va4.checked) {
+        permataVa.textContent = textNominalpermataVa + 'Rp.' + harga;
+        virtualAcc.textContent = textNominalpermataVa + 'Rp.' + harga;
+        harga = 0; // set ulang biar g nimpa 
+    }
+})
+
+// 5
+const permata_Va5 = document.getElementById('5');
+permata_Va5.addEventListener('click', () => {
+    harga += 117000;
+
+    if (permata_Va5.checked) {
+        permataVa.textContent = textNominalpermataVa + 'Rp.' + harga;
+        virtualAcc.textContent = textNominalpermataVa + 'Rp.' + harga;
+        harga = 0; // set ulang biar g nimpa 
+    }
+})
+
+// 6
+const permata_Va6 = document.getElementById('6');
+permata_Va6.addEventListener('click', () => {
+    harga += 117000;
+
+    if (permata_Va6.checked) {
+        permataVa.textContent = textNominalpermataVa + 'Rp.' + harga;
+        virtualAcc.textContent = textNominalpermataVa + 'Rp.' + harga;
+        harga = 0; // set ulang biar g nimpa 
+    }
+})
+
+// 7
+const permata_Va7 = document.getElementById('7');
+permata_Va7.addEventListener('click', () => {
+    harga += 117000;
+
+    if (permata_Va7.checked) {
+        permataVa.textContent = textNominalpermataVa + 'Rp.' + harga;
+        virtualAcc.textContent = textNominalpermataVa + 'Rp.' + harga;
+        harga = 0; // set ulang biar g nimpa 
+    }
+})
+
+// 8
+const permata_Va8 = document.getElementById('8');
+permata_Va8.addEventListener('click', () => {
+    harga += 117000;
+
+    if (permata_Va8.checked) {
+        permataVa.textContent = textNominalpermataVa + 'Rp.' + harga;
+        virtualAcc.textContent = textNominalpermataVa + 'Rp.' + harga;
+        harga = 0; // set ulang biar g nimpa 
+    }
+})
+
+// 9
+const permata_Va9 = document.getElementById('9');
+permata_Va9.addEventListener('click', () => {
+    harga += 117000;
+
+    if (permata_Va9.checked) {
+        permataVa.textContent = textNominalpermataVa + 'Rp.' + harga;
+        virtualAcc.textContent = textNominalpermataVa + 'Rp.' + harga;
+        harga = 0; // set ulang biar g nimpa 
+    }
+})
+
+// 10
+const permata_Va10 = document.getElementById('10');
+permata_Va10.addEventListener('click', () => {
+    harga += 117000;
+
+    if (permata_Va10.checked) {
+        permataVa.textContent = textNominalpermataVa + 'Rp.' + harga;
+        virtualAcc.textContent = textNominalpermataVa + 'Rp.' + harga;
+        harga = 0; // set ulang biar g nimpa 
+    }
+})
+
+// 11
+const permata_Va11 = document.getElementById('11');
+permata_Va11.addEventListener('click', () => {
+    harga += 117000;
+
+    if (permata_Va11.checked) {
+        permataVa.textContent = textNominalpermataVa + 'Rp.' + harga;
+        virtualAcc.textContent = textNominalpermataVa + 'Rp.' + harga;
+        harga = 0; // set ulang biar g nimpa 
+    }
+})
+
+// 12
+const permata_Va12 = document.getElementById('12');
+permata_Va12.addEventListener('click', () => {
+    harga += 117000;
+
+    if (permata_Va12.checked) {
+        permataVa.textContent = textNominalpermataVa + 'Rp.' + harga;
+        virtualAcc.textContent = textNominalpermataVa + 'Rp.' + harga;
+        harga = 0; // set ulang biar g nimpa 
+    }
+})
+
+// 13
+const permata_Va13 = document.getElementById('13');
+permata_Va13.addEventListener('click', () => {
+    harga += 117000;
+
+    if (permata_Va13.checked) {
+        permataVa.textContent = textNominalpermataVa + 'Rp.' + harga;
+        virtualAcc.textContent = textNominalpermataVa + 'Rp.' + harga;
+        harga = 0; // set ulang biar g nimpa 
+    }
+})
+
+// 14
+const permata_Va14 = document.getElementById('14');
+permata_Va14.addEventListener('click', () => {
+    harga += 117000;
+
+    if (permata_Va14.checked) {
+        permataVa.textContent = textNominalpermataVa + 'Rp.' + harga;
+        virtualAcc.textContent = textNominalpermataVa + 'Rp.' + harga;
+        harga = 0; // set ulang biar g nimpa 
+    }
+})
+
+// 15
+const permata_Va15 = document.getElementById('15');
+permata_Va15.addEventListener('click', () => {
+    harga += 117000;
+
+    if (permata_Va15.checked) {
+        permataVa.textContent = textNominalpermataVa + 'Rp.' + harga;
+        virtualAcc.textContent = textNominalpermataVa + 'Rp.' + harga;
+        harga = 0; // set ulang biar g nimpa 
+    }
+})
+
+// 16
+const permata_Va16 = document.getElementById('16');
+permata_Va16.addEventListener('click', () => {
+    harga += 117000;
+
+    if (permata_Va16.checked) {
+        permataVa.textContent = textNominalpermataVa + 'Rp.' + harga;
+        virtualAcc.textContent = textNominalpermataVa + 'Rp.' + harga;
+        harga = 0; // set ulang biar g nimpa 
+    }
+})
+
+// 17
+const permata_Va17 = document.getElementById('17');
+permata_Va17.addEventListener('click', () => {
+    harga += 117000;
+
+    if (permata_Va17.checked) {
+        permataVa.textContent = textNominalpermataVa + 'Rp.' + harga;
+        virtualAcc.textContent = textNominalpermataVa + 'Rp.' + harga;
+        harga = 0; // set ulang biar g nimpa 
+    }
+})
+
+// 18
+const permata_Va18 = document.getElementById('18');
+permata_Va18.addEventListener('click', () => {
+    harga += 117000;
+
+    if (permata_Va18.checked) {
+        permataVa.textContent = textNominalpermataVa + 'Rp.' + harga;
+        virtualAcc.textContent = textNominalpermataVa + 'Rp.' + harga;
         harga = 0; // set ulang biar g nimpa 
     }
 })
