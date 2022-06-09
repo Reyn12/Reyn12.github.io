62db2f406b00d2f1b3afc4a769994287713560d6
Rizki Muh Pasinringi <rizkimuhpasinringi@gmail.com>
2022-06-09 22:26:15 +0800

    merge

---
+++ Js/All/checkBox.js
@@ -0,0 +1,13 @@
+// window.onscroll = function () {
+//     const header = document.querySelector('nav');
+//     const fixedNav = header.offsetTop;
+//     const checkBox = document.querySelector('#checkBox');
+  
+//     if (window.pageYOffset > fixedNav) {
+//       checkBox.classList.remove('hidden');
+//       checkBox.classList.add('flex');
+//     } else {
+//       checkBox.classList.remove('flex');
+//       checkBox.classList.add('hidden');
+//     }
+//   };
