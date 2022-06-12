c5b7cb3f9a23f70377b56668204b9486e2164104
Rizki Muh Pasinringi <rizkimuhpasinringi@gmail.com>
2022-06-12 10:04:34 +0800

    merge 'rizki

---
+++ Js/MobileLegends/Conflig/noInput.js
@@ -0,0 +1,8 @@
+const Order = document.getElementById('Order')
+const ml_starlight = document.querySelector('button#Order')
+
+Order.addEventListener('click', () => {
+    if (ml_starlight == checked()) {
+        alert("lu")
+    }
+})
