ff7a6424e36e1faaf9aa851f528e55f64cf894bc
Muhammad Renaldi <muhammadrenaldi687@gmail.com>
2022-07-31 19:52:23 +0700

    fix bug ovo.js

--- Js/MobileLegends/MetodePembayaran/Ewallet/ovo.js
+++ Js/MobileLegends/MetodePembayaran/Ewallet/ovo.js
@@ -1,221 +1,221 @@
-let eWallet = document.getElementById('e_Wallet');
+let eWallet = document.getElementById("e_Wallet");
 
 // mayBankVa
-const ovoEwallet = document.getElementById('ovoEwallet');
+const ovoEwallet = document.getElementById("ovoEwallet");
 const textNominalOvoEwallet = ovoEwallet.textContent; // biar presist
 
 // 1
-const ovoEwallet_1 = document.getElementById('ml_starlight');
-ovoEwallet_1.addEventListener('click', () => {
-    harga = '120.000';
+const ovoEwallet_1 = document.getElementById("ml_starlight");
+ovoEwallet_1.addEventListener("click", () => {
+  harga = "120.000";
 
-    if (ovoEwallet_1.checked) {
-        ovoEwallet.textContent = textNominalOvoEwallet + 'Rp ' + harga;
-        eWallet.textContent = textNominalOvoEwallet + 'Rp ' + harga;
-        harga = 0; // set ulang biar g nimpa 
-    }
-})
+  if (ovoEwallet_1.checked) {
+    ovoEwallet.textContent = textNominalOvoEwallet + "Rp " + harga;
+    eWallet.textContent = `${textNominalOvoEwallet}Rp ${harga}`;
+    harga = 0; // set ulang biar g nimpa
+  }
+});
 
 // 2
-const ovoEwallet_2 = document.getElementById('ml_starlightPlus');
-ovoEwallet_2.addEventListener('click', () => {
-    harga = '320.000';
+const ovoEwallet_2 = document.getElementById("ml_starlightPlus");
+ovoEwallet_2.addEventListener("click", () => {
+  harga = "320.000";
 
-    if (ovoEwallet_2.checked) {
-        ovoEwallet.textContent = textNominalOvoEwallet + 'Rp ' + harga;
-        eWallet.textContent = textNominalOvoEwallet + 'Rp ' + harga;
-        harga = 0; // set ulang biar g nimpa 
-    }
-})
+  if (ovoEwallet_2.checked) {
+    ovoEwallet.textContent = textNominalOvoEwallet + "Rp " + harga;
+    eWallet.textContent = textNominalOvoEwallet + "Rp " + harga;
+    harga = 0; // set ulang biar g nimpa
+  }
+});
 
 // 3
-const maybank_Va3 = document.getElementById('ml_twilight');
-maybank_Va3.addEventListener('click', () => {
-    harga = '140.500';
+const ovoEwallet_3 = document.getElementById("ml_twilight");
+ovoEwallet_3.addEventListener("click", () => {
+  harga = "140.500";
 
-    if (maybank_Va3.checked) {
-        ovoEwallet.textContent = textNominalOvoEwallet + 'Rp ' + harga;
-        eWallet.textContent = textNominalOvoEwallet + 'Rp ' + harga;
-        harga = 0; // set ulang biar g nimpa 
-    }
-})
+  if (ovoEwallet_3.checked) {
+    ovoEwallet.textContent = textNominalOvoEwallet + "Rp " + harga;
+    eWallet.textContent = textNominalOvoEwallet + "Rp " + harga;
+    harga = 0; // set ulang biar g nimpa
+  }
+});
 
 // 4
-const maybank_Va4 = document.getElementById('ml_86');
-maybank_Va4.addEventListener('click', () => {
-    harga = '22.600';
+const ovoEwallet_4 = document.getElementById("ml_86");
+ovoEwallet_4.addEventListener("click", () => {
+  harga = "22.600";
 
-    if (maybank_Va4.checked) {
-        ovoEwallet.textContent = textNominalOvoEwallet + 'Rp ' + harga;
-        eWallet.textContent = textNominalOvoEwallet + 'Rp ' + harga;
-        harga = 0; // set ulang biar g nimpa 
-    }
-})
+  if (ovoEwallet_4.checked) {
+    ovoEwallet.textContent = textNominalOvoEwallet + "Rp " + harga;
+    eWallet.textContent = textNominalOvoEwallet + "Rp " + harga;
+    harga = 0; // set ulang biar g nimpa
+  }
+});
 
 // 5
-const maybank_Va5 = document.getElementById('ml_172');
-maybank_Va5.addEventListener('click', () => {
-    harga = '45.800';
+const ovoEwallet_5 = document.getElementById("ml_172");
+ovoEwallet_5.addEventListener("click", () => {
+  harga = "45.800";
 
-    if (maybank_Va5.checked) {
-        ovoEwallet.textContent = textNominalOvoEwallet + 'Rp ' + harga;
-        eWallet.textContent = textNominalOvoEwallet + 'Rp ' + harga;
-        harga = 0; // set ulang biar g nimpa 
-    }
-})
+  if (ovoEwallet_5.checked) {
+    ovoEwallet.textContent = textNominalOvoEwallet + "Rp " + harga;
+    eWallet.textContent = textNominalOvoEwallet + "Rp " + harga;
+    harga = 0; // set ulang biar g nimpa
+  }
+});
 
 // 6
-const maybank_Va6 = document.getElementById('ml_257');
-maybank_Va6.addEventListener('click', () => {
-    harga = '65.700';
+const ovoEwallet_6 = document.getElementById("ml_257");
+ovoEwallet_6.addEventListener("click", () => {
+  harga = "65.700";
 
-    if (maybank_Va6.checked) {
-        ovoEwallet.textContent = textNominalOvoEwallet + 'Rp ' + harga;
-        eWallet.textContent = textNominalOvoEwallet + 'Rp ' + harga;
-        harga = 0; // set ulang biar g nimpa 
-    }
-})
+  if (ovoEwallet_6.checked) {
+    ovoEwallet.textContent = textNominalOvoEwallet + "Rp " + harga;
+    eWallet.textContent = textNominalOvoEwallet + "Rp " + harga;
+    harga = 0; // set ulang biar g nimpa
+  }
+});
 
 // 7
-const maybank_Va7 = document.getElementById('ml_344');
-maybank_Va7.addEventListener('click', () => {
-    harga = '85.600';
+const ovoEwallet_7 = document.getElementById("ml_344");
+ovoEwallet_7.addEventListener("click", () => {
+  harga = "85.600";
 
-    if (maybank_Va7.checked) {
-        ovoEwallet.textContent = textNominalOvoEwallet + 'Rp ' + harga;
-        eWallet.textContent = textNominalOvoEwallet + 'Rp ' + harga;
-        harga = 0; // set ulang biar g nimpa 
-    }
-})
+  if (ovoEwallet_7.checked) {
+    ovoEwallet.textContent = textNominalOvoEwallet + "Rp " + harga;
+    eWallet.textContent = textNominalOvoEwallet + "Rp " + harga;
+    harga = 0; // set ulang biar g nimpa
+  }
+});
 
 // 8
-const maybank_Va8 = document.getElementById('ml_429');
-maybank_Va8.addEventListener('click', () => {
-    harga = '108.900';
+const ovoEwallet_8 = document.getElementById("ml_429");
+ovoEwallet_8.addEventListener("click", () => {
+  harga = "108.900";
 
-    if (maybank_Va8.checked) {
-        ovoEwallet.textContent = textNominalOvoEwallet + 'Rp ' + harga;
-        eWallet.textContent = textNominalOvoEwallet + 'Rp ' + harga;
-        harga = 0; // set ulang biar g nimpa 
-    }
-})
+  if (ovoEwallet_8.checked) {
+    ovoEwallet.textContent = textNominalOvoEwallet + "Rp " + harga;
+    eWallet.textContent = textNominalOvoEwallet + "Rp " + harga;
+    harga = 0; // set ulang biar g nimpa
+  }
+});
 
 // 9
-const maybank_Va9 = document.getElementById('ml_556');
-maybank_Va9.addEventListener('click', () => {
-    harga = '138.760';
+const ovoEwallet_9 = document.getElementById("ml_556");
+ovoEwallet_9.addEventListener("click", () => {
+  harga = "138.760";
 
-    if (maybank_Va9.checked) {
-        ovoEwallet.textContent = textNominalOvoEwallet + 'Rp ' + harga;
-        eWallet.textContent = textNominalOvoEwallet + 'Rp ' + harga;
-        harga = 0; // set ulang biar g nimpa 
-    }
-})
+  if (ovoEwallet_9.checked) {
+    ovoEwallet.textContent = textNominalOvoEwallet + "Rp " + harga;
+    eWallet.textContent = textNominalOvoEwallet + "Rp " + harga;
+    harga = 0; // set ulang biar g nimpa
+  }
+});
 
 // 10
-const maybank_Va10 = document.getElementById('ml_706');
-maybank_Va10.addEventListener('click', () => {
-    harga = '168.200';
+const ovoEwallet_10 = document.getElementById("ml_706");
+ovoEwallet_10.addEventListener("click", () => {
+  harga = "168.200";
 
-    if (maybank_Va10.checked) {
-        ovoEwallet.textContent = textNominalOvoEwallet + 'Rp ' + harga;
-        eWallet.textContent = textNominalOvoEwallet + 'Rp ' + harga;
-        harga = 0; // set ulang biar g nimpa 
-    }
-})
+  if (ovoEwallet_10.checked) {
+    ovoEwallet.textContent = textNominalOvoEwallet + "Rp " + harga;
+    eWallet.textContent = textNominalOvoEwallet + "Rp " + harga;
+    harga = 0; // set ulang biar g nimpa
+  }
+});
 
 // 11
-const maybank_Va11 = document.getElementById('ml_720');
-maybank_Va11.addEventListener('click', () => {
-    harga = '170.800';
+const ovoEwallet_11 = document.getElementById("ml_720");
+ovoEwallet_11.addEventListener("click", () => {
+  harga = "170.800";
 
-    if (maybank_Va11.checked) {
-        ovoEwallet.textContent = textNominalOvoEwallet + 'Rp ' + harga;
-        eWallet.textContent = textNominalOvoEwallet + 'Rp ' + harga;
-        harga = 0; // set ulang biar g nimpa 
-    }
-})
+  if (ovoEwallet_11.checked) {
+    ovoEwallet.textContent = textNominalOvoEwallet + "Rp " + harga;
+    eWallet.textContent = textNominalOvoEwallet + "Rp " + harga;
+    harga = 0; // set ulang biar g nimpa
+  }
+});
 
 // 12
-const maybank_Va12 = document.getElementById('ml_878');
-maybank_Va12.addEventListener('click', () => {
-    harga = '208.600';
+const ovoEwallet_12 = document.getElementById("ml_878");
+ovoEwallet_12.addEventListener("click", () => {
+  harga = "208.600";
 
-    if (maybank_Va12.checked) {
-        ovoEwallet.textContent = textNominalOvoEwallet + 'Rp ' + harga;
-        eWallet.textContent = textNominalOvoEwallet + 'Rp ' + harga;
-        harga = 0; // set ulang biar g nimpa 
-    }
-})
+  if (ovoEwallet_12.checked) {
+    ovoEwallet.textContent = textNominalOvoEwallet + "Rp " + harga;
+    eWallet.textContent = textNominalOvoEwallet + "Rp " + harga;
+    harga = 0; // set ulang biar g nimpa
+  }
+});
 
 // 13
-const maybank_Va13 = document.getElementById('ml_977');
-maybank_Va13.addEventListener('click', () => {
-    harga = '235.400';
+const ovoEwallet_13 = document.getElementById("ml_977");
+ovoEwallet_13.addEventListener("click", () => {
+  harga = "235.400";
 
-    if (maybank_Va13.checked) {
-        ovoEwallet.textContent = textNominalOvoEwallet + 'Rp ' + harga;
-        eWallet.textContent = textNominalOvoEwallet + 'Rp ' + harga;
-        harga = 0; // set ulang biar g nimpa 
-    }
-})
+  if (ovoEwallet_13.checked) {
+    ovoEwallet.textContent = textNominalOvoEwallet + "Rp " + harga;
+    eWallet.textContent = textNominalOvoEwallet + "Rp " + harga;
+    harga = 0; // set ulang biar g nimpa
+  }
+});
 
 // 14
-const maybank_Va14 = document.getElementById('ml_1163');
-maybank_Va14.addEventListener('click', () => {
-    harga = '278.800';
+const ovoEwallet_14 = document.getElementById("ml_1163");
+ovoEwallet_14.addEventListener("click", () => {
+  harga = "278.800";
 
-    if (maybank_Va14.checked) {
-        ovoEwallet.textContent = textNominalOvoEwallet + 'Rp ' + harga;
-        eWallet.textContent = textNominalOvoEwallet + 'Rp ' + harga;
-        harga = 0; // set ulang biar g nimpa 
-    }
-})
+  if (ovoEwallet_14.checked) {
+    ovoEwallet.textContent = textNominalOvoEwallet + "Rp " + harga;
+    eWallet.textContent = textNominalOvoEwallet + "Rp " + harga;
+    harga = 0; // set ulang biar g nimpa
+  }
+});
 
 // 15
-const maybank_Va15 = document.getElementById('ml_1412');
-maybank_Va15.addEventListener('click', () => {
-    harga = '332.860';
+const ovoEwallet_15 = document.getElementById("ml_1412");
+ovoEwallet_15.addEventListener("click", () => {
+  harga = "332.860";
 
-    if (maybank_Va15.checked) {
-        ovoEwallet.textContent = textNominalOvoEwallet + 'Rp ' + harga;
-        eWallet.textContent = textNominalOvoEwallet + 'Rp ' + harga;
-        harga = 0; // set ulang biar g nimpa 
-    }
-})
+  if (ovoEwallet_15.checked) {
+    ovoEwallet.textContent = textNominalOvoEwallet + "Rp " + harga;
+    eWallet.textContent = textNominalOvoEwallet + "Rp " + harga;
+    harga = 0; // set ulang biar g nimpa
+  }
+});
 
 // 16
-const maybank_Va16 = document.getElementById('ml_4394');
-maybank_Va16.addEventListener('click', () => {
-    harga = '978.500';
+const ovoEwallet_16 = document.getElementById("ml_4394");
+ovoEwallet_16.addEventListener("click", () => {
+  harga = "978.500";
 
-    if (maybank_Va16.checked) {
-        ovoEwallet.textContent = textNominalOvoEwallet + 'Rp ' + harga;
-        eWallet.textContent = textNominalOvoEwallet + 'Rp ' + harga;
-        harga = 0; // set ulang biar g nimpa 
-    }
-})
+  if (ovoEwallet_16.checked) {
+    ovoEwallet.textContent = textNominalOvoEwallet + "Rp " + harga;
+    eWallet.textContent = textNominalOvoEwallet + "Rp " + harga;
+    harga = 0; // set ulang biar g nimpa
+  }
+});
 
 // 17
-const maybank_Va17 = document.getElementById('ml_5534');
-maybank_Va17.addEventListener('click', () => {
-    harga = '1.248.100';
+const ovoEwallet_17 = document.getElementById("ml_5534");
+ovoEwallet_17.addEventListener("click", () => {
+  harga = "1.248.100";
 
-    if (maybank_Va17.checked) {
-        ovoEwallet.textContent = textNominalOvoEwallet + 'Rp ' + harga;
-        eWallet.textContent = textNominalOvoEwallet + 'Rp ' + harga;
-        harga = 0; // set ulang biar g nimpa 
-    }
-})
+  if (ovoEwallet_17.checked) {
+    ovoEwallet.textContent = textNominalOvoEwallet + "Rp " + harga;
+    eWallet.textContent = textNominalOvoEwallet + "Rp " + harga;
+    harga = 0; // set ulang biar g nimpa
+  }
+});
 
 // 18
-const maybank_Va18 = document.getElementById('ml_9302');
-maybank_Va18.addEventListener('click', () => {
-    harga = "2.075.700";
+const ovoEwallet_18 = document.getElementById("ml_9302");
+ovoEwallet_18.addEventListener("click", () => {
+  harga = "2.075.700";
 
-    if (maybank_Va18.checked) {
-        ovoEwallet.textContent = textNominalOvoEwallet + 'Rp ' + harga;
-        eWallet.textContent = textNominalOvoEwallet + 'Rp ' + harga;
-        harga = 0; // set ulang biar g nimpa 
-    }
-})
+  if (ovoEwallet_18.checked) {
+    ovoEwallet.textContent = textNominalOvoEwallet + "Rp " + harga;
+    eWallet.textContent = textNominalOvoEwallet + "Rp " + harga;
+    harga = 0; // set ulang biar g nimpa
+  }
+});
--- Js/MobileLegends/MetodePembayaran/VirtualAcc/mandiriVa.js
+++ Js/MobileLegends/MetodePembayaran/VirtualAcc/mandiriVa.js
@@ -1,202 +1,200 @@
-// Virtual Account
-// mandiriVa
-const mandiriVa = document.getElementById('mandiriVa');
+const mandiriVa = document.getElementById("mandiriVa");
 const textNominalmandiriVa = mandiriVa.textContent; // biar presist
 
 // 1
-const mandiri_Va = document.getElementById('ml_starlight');
-mandiri_Va.addEventListener('click', () => {
-    harga = '120.000';
+const mandiri_Va1 = document.getElementById("ml_starlight");
+mandiri_Va1.addEventListener("click", () => {
+  harga = "120.000";
 
-    if (mandiri_Va.checked) {
-        mandiriVa.textContent = textNominalmandiriVa + 'Rp ' + harga;
-        harga = 0; // set ulang biar g nimpa 
-    }
-})
+  if (mandiri_Va1.checked) {
+    mandiriVa.textContent = textNominalmandiriVa + "Rp " + harga;
+    harga = 0; // set ulang biar g nimpa
+  }
+});
 
 // 2
-const mandiri_Va2 = document.getElementById('ml_starlightPlus');
-mandiri_Va2.addEventListener('click', () => {
-    harga = '320.000';
+const mandiri_Va2 = document.getElementById("ml_starlightPlus");
+mandiri_Va2.addEventListener("click", () => {
+  harga = "320.000";
 
-    if (mandiri_Va2.checked) {
-        mandiriVa.textContent = textNominalmandiriVa + 'Rp ' + harga;
-        harga = 0; // set ulang biar g nimpa 
-    }
-})
+  if (mandiri_Va2.checked) {
+    mandiriVa.textContent = textNominalmandiriVa + "Rp " + harga;
+    harga = 0; // set ulang biar g nimpa
+  }
+});
 
 // 3
-const mandiri_Va3 = document.getElementById('ml_twilight');
-mandiri_Va3.addEventListener('click', () => {
-    harga = '140.500';
+const mandiri_Va3 = document.getElementById("ml_twilight");
+mandiri_Va3.addEventListener("click", () => {
+  harga = "140.500";
 
-    if (mandiri_Va3.checked) {
-        mandiriVa.textContent = textNominalmandiriVa + 'Rp ' + harga;
-        harga = 0; // set ulang biar g nimpa 
-    }
-})
+  if (mandiri_Va3.checked) {
+    mandiriVa.textContent = textNominalmandiriVa + "Rp " + harga;
+    harga = 0; // set ulang biar g nimpa
+  }
+});
 
 // 4
-const mandiri_Va4 = document.getElementById('ml_86');
-mandiri_Va4.addEventListener('click', () => {
-    harga = '22.600';
+const mandiri_Va4 = document.getElementById("ml_86");
+mandiri_Va4.addEventListener("click", () => {
+  harga = "22.600";
 
-    if (mandiri_Va4.checked) {
-        mandiriVa.textContent = textNominalmandiriVa + 'Rp ' + harga;
-        harga = 0; // set ulang biar g nimpa 
-    }
-})
+  if (mandiri_Va4.checked) {
+    mandiriVa.textContent = textNominalmandiriVa + "Rp " + harga;
+    harga = 0; // set ulang biar g nimpa
+  }
+});
 
 // 5
-const mandiri_Va5 = document.getElementById('ml_172');
-mandiri_Va5.addEventListener('click', () => {
-    harga = '45.800';
+const mandiri_Va5 = document.getElementById("ml_172");
+mandiri_Va5.addEventListener("click", () => {
+  harga = "45.800";
 
-    if (mandiri_Va5.checked) {
-        mandiriVa.textContent = textNominalmandiriVa + 'Rp ' + harga;
-        harga = 0; // set ulang biar g nimpa 
-    }
-})
+  if (mandiri_Va5.checked) {
+    mandiriVa.textContent = textNominalmandiriVa + "Rp " + harga;
+    harga = 0; // set ulang biar g nimpa
+  }
+});
 
 // 6
-const mandiri_Va6 = document.getElementById('ml_257');
-mandiri_Va6.addEventListener('click', () => {
-    harga = '65.700';
+const mandiri_Va6 = document.getElementById("ml_257");
+mandiri_Va6.addEventListener("click", () => {
+  harga = "65.700";
 
-    if (mandiri_Va6.checked) {
-        mandiriVa.textContent = textNominalmandiriVa + 'Rp ' + harga;
-        harga = 0; // set ulang biar g nimpa 
-    }
-})
+  if (mandiri_Va6.checked) {
+    mandiriVa.textContent = textNominalmandiriVa + "Rp " + harga;
+    harga = 0; // set ulang biar g nimpa
+  }
+});
 
 // 7
-const mandiri_Va7 = document.getElementById('ml_344');
-mandiri_Va7.addEventListener('click', () => {
-    harga = '85.600';
+const mandiri_Va7 = document.getElementById("ml_344");
+mandiri_Va7.addEventListener("click", () => {
+  harga = "85.600";
 
-    if (mandiri_Va7.checked) {
-        mandiriVa.textContent = textNominalmandiriVa + 'Rp ' + harga;
-        harga = 0; // set ulang biar g nimpa 
-    }
-})
+  if (mandiri_Va7.checked) {
+    mandiriVa.textContent = textNominalmandiriVa + "Rp " + harga;
+    harga = 0; // set ulang biar g nimpa
+  }
+});
 
 // 8
-const mandiri_Va8 = document.getElementById('ml_429');
-mandiri_Va8.addEventListener('click', () => {
-    harga = '108.900';
+const mandiri_Va8 = document.getElementById("ml_429");
+mandiri_Va8.addEventListener("click", () => {
+  harga = "108.900";
 
-    if (mandiri_Va8.checked) {
-        mandiriVa.textContent = textNominalmandiriVa + 'Rp ' + harga;
-        harga = 0; // set ulang biar g nimpa 
-    }
-})
+  if (mandiri_Va8.checked) {
+    mandiriVa.textContent = textNominalmandiriVa + "Rp " + harga;
+    harga = 0; // set ulang biar g nimpa
+  }
+});
 
 // 9
-const mandiri_Va9 = document.getElementById('ml_556');
-mandiri_Va9.addEventListener('click', () => {
-    harga = '138.760';
+const mandiri_Va9 = document.getElementById("ml_556");
+mandiri_Va9.addEventListener("click", () => {
+  harga = "138.760";
 
-    if (mandiri_Va9.checked) {
-        mandiriVa.textContent = textNominalmandiriVa + 'Rp ' + harga;
-        harga = 0; // set ulang biar g nimpa 
-    }
-})
+  if (mandiri_Va9.checked) {
+    mandiriVa.textContent = textNominalmandiriVa + "Rp " + harga;
+    harga = 0; // set ulang biar g nimpa
+  }
+});
 
 // 10
-const mandiri_Va10 = document.getElementById('ml_706');
-mandiri_Va10.addEventListener('click', () => {
-    harga = '168.200';
+const mandiri_Va10 = document.getElementById("ml_706");
+mandiri_Va10.addEventListener("click", () => {
+  harga = "168.200";
 
-    if (mandiri_Va10.checked) {
-        mandiriVa.textContent = textNominalmandiriVa + 'Rp ' + harga;
-        harga = 0; // set ulang biar g nimpa 
-    }
-})
+  if (mandiri_Va10.checked) {
+    mandiriVa.textContent = textNominalmandiriVa + "Rp " + harga;
+    harga = 0; // set ulang biar g nimpa
+  }
+});
 
 // 11
-const mandiri_Va11 = document.getElementById('ml_720');
-mandiri_Va11.addEventListener('click', () => {
-    harga = '170.800';
+const mandiri_Va11 = document.getElementById("ml_720");
+mandiri_Va11.addEventListener("click", () => {
+  harga = "170.800";
 
-    if (mandiri_Va11.checked) {
-        mandiriVa.textContent = textNominalmandiriVa + 'Rp ' + harga;
-        harga = 0; // set ulang biar g nimpa 
-    }
-})
+  if (mandiri_Va11.checked) {
+    mandiriVa.textContent = textNominalmandiriVa + "Rp " + harga;
+    harga = 0; // set ulang biar g nimpa
+  }
+});
 
 // 12
-const mandiri_Va12 = document.getElementById('ml_878');
-mandiri_Va12.addEventListener('click', () => {
-    harga = '208.600';
+const mandiri_Va12 = document.getElementById("ml_878");
+mandiri_Va12.addEventListener("click", () => {
+  harga = "208.600";
 
-    if (mandiri_Va12.checked) {
-        mandiriVa.textContent = textNominalmandiriVa + 'Rp ' + harga;
-        harga = 0; // set ulang biar g nimpa 
-    }
-})
+  if (mandiri_Va12.checked) {
+    mandiriVa.textContent = textNominalmandiriVa + "Rp " + harga;
+    harga = 0; // set ulang biar g nimpa
+  }
+});
 
 // 13
-const mandiri_Va13 = document.getElementById('ml_977');
-mandiri_Va13.addEventListener('click', () => {
-    harga = '235.400';
+const mandiri_Va13 = document.getElementById("ml_977");
+mandiri_Va13.addEventListener("click", () => {
+  harga = "235.400";
 
-    if (mandiri_Va13.checked) {
-        mandiriVa.textContent = textNominalmandiriVa + 'Rp ' + harga;
-        harga = 0; // set ulang biar g nimpa 
-    }
-})
+  if (mandiri_Va13.checked) {
+    mandiriVa.textContent = textNominalmandiriVa + "Rp " + harga;
+    harga = 0; // set ulang biar g nimpa
+  }
+});
 
 // 14
-const mandiri_Va14 = document.getElementById('ml_1163');
-mandiri_Va14.addEventListener('click', () => {
-    harga = '278.800';
+const mandiri_Va14 = document.getElementById("ml_1163");
+mandiri_Va14.addEventListener("click", () => {
+  harga = "278.800";
 
-    if (mandiri_Va14.checked) {
-        mandiriVa.textContent = textNominalmandiriVa + 'Rp ' + harga;
-        harga = 0; // set ulang biar g nimpa 
-    }
-})
+  if (mandiri_Va14.checked) {
+    mandiriVa.textContent = textNominalmandiriVa + "Rp " + harga;
+    harga = 0; // set ulang biar g nimpa
+  }
+});
 
 // 15
-const mandiri_Va15 = document.getElementById('ml_1412');
-mandiri_Va15.addEventListener('click', () => {
-    harga = '332.860';
+const mandiri_Va15 = document.getElementById("ml_1412");
+mandiri_Va15.addEventListener("click", () => {
+  harga = "332.860";
 
-    if (mandiri_Va15.checked) {
-        mandiriVa.textContent = textNominalmandiriVa + 'Rp ' + harga;
-        harga = 0; // set ulang biar g nimpa 
-    }
-})
+  if (mandiri_Va15.checked) {
+    mandiriVa.textContent = textNominalmandiriVa + "Rp " + harga;
+    harga = 0; // set ulang biar g nimpa
+  }
+});
 
 // 16
-const mandiri_Va16 = document.getElementById('ml_4394');
-mandiri_Va16.addEventListener('click', () => {
-    harga = '978.500';
+const mandiri_Va16 = document.getElementById("ml_4394");
+mandiri_Va16.addEventListener("click", () => {
+  harga = "978.500";
 
-    if (mandiri_Va16.checked) {
-        mandiriVa.textContent = textNominalmandiriVa + 'Rp ' + harga;
-        harga = 0; // set ulang biar g nimpa 
-    }
-})
+  if (mandiri_Va16.checked) {
+    mandiriVa.textContent = textNominalmandiriVa + "Rp " + harga;
+    harga = 0; // set ulang biar g nimpa
+  }
+});
 
 // 17
-const mandiri_Va17 = document.getElementById('ml_5534');
-mandiri_Va17.addEventListener('click', () => {
-    harga = '1.248.100';
+const mandiri_Va17 = document.getElementById("ml_5534");
+mandiri_Va17.addEventListener("click", () => {
+  harga = "1.248.100";
 
-    if (mandiri_Va17.checked) {
-        mandiriVa.textContent = textNominalmandiriVa + 'Rp ' + harga;
-        harga = 0; // set ulang biar g nimpa 
-    }
-})
+  if (mandiri_Va17.checked) {
+    mandiriVa.textContent = textNominalmandiriVa + "Rp " + harga;
+    harga = 0; // set ulang biar g nimpa
+  }
+});
 
 // 18
-const mandiri_Va18 = document.getElementById('ml_9302');
-mandiri_Va18.addEventListener('click', () => {
-    harga = "2.075.700";
+const mandiri_Va18 = document.getElementById("ml_9302");
+mandiri_Va18.addEventListener("click", () => {
+  harga = "2.075.700";
 
-    if (mandiri_Va18.checked) {
-        mandiriVa.textContent = textNominalmandiriVa + 'Rp ' + harga;
-        harga = 0; // set ulang biar g nimpa 
-    }
-})
+  if (mandiri_Va18.checked) {
+    mandiriVa.textContent = textNominalmandiriVa + "Rp " + harga;
+    harga = 0; // set ulang biar g nimpa
+  }
+});
